003b1dc8dfda3c3efe8a585138b1d94a0bf10016
nahkd123 <nahkd123@gmail.com>
2023-08-28 21:39:39 +0700

    Attach event the intended way + use pointer events

--- modules/ui/src/components/windows/WindowsContainer.vue
+++ modules/ui/src/components/windows/WindowsContainer.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
 import { traverse } from '@/utils';
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const containerSlot = ref<HTMLDivElement>();
 
-onMounted(() => {
-    containerSlot.value?.addEventListener("mousedown", event => {
-        const window = traverse(
-            event.target as HTMLElement,
-            v => v.classList.contains("window") || v == containerSlot.value,
-            v => v.parentElement
-        );
-
-        if (window == containerSlot.value || window == null) return;
-        if (containerSlot.value?.lastElementChild == window) return;
-        containerSlot.value?.appendChild(window);
-    });
-});
+function onPointerDown(event: PointerEvent) {
+    const window = traverse(
+        event.target as HTMLElement,
+        v => v.classList.contains("window") || v == containerSlot.value,
+        v => v.parentElement
+    )
+    if (window == containerSlot.value || window == null) return;
+    if (containerSlot.value?.lastElementChild == window) return;
+    containerSlot.value?.appendChild(window);
+}
 </script>
 
 <template>
-    <div class="container" ref="containerSlot">
+    <div class="container" ref="containerSlot" @pointerdown="onPointerDown">
         <slot></slot>
     </div>
 </template>
