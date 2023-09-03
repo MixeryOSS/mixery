ef7fa682736a56a7d82dcc94d09be20544f6f470
nahkd123 <nahkd123@gmail.com>
2023-09-03 18:19:41 +0700

    Add "Double click to type value"

--- modules/ui/src/components/workspace/nodes/ControlSlider.vue
+++ modules/ui/src/components/workspace/nodes/ControlSlider.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useParentState, useTrackableXYv2 } from '@/components/composes';
+import { DoubleClickHandler } from '@/handling/DoubleClickHandler';
+import { ref } from 'vue';
 
 const props = defineProps<{
     modelValue: number
@@ -7,15 +9,44 @@ const props = defineProps<{
 const emits = defineEmits(["update:modelValue"]);
 
 const value = useParentState<number>("modelValue", props, emits);
-const onPointerDown = useTrackableXYv2(value, undefined, {
+const textMode = ref(false);
+
+const trackPosition = useTrackableXYv2(value, undefined, {
     scale: 0.1,
     shiftScale: 0.01,
     ctrlScale: 1
 });
+
+const doubleclick = new DoubleClickHandler(() => 500, () => {
+    textMode.value = true;
+});
+
+function onPointerDown(event: PointerEvent) {
+    trackPosition(event);
+    doubleclick.mouseDown(event);
+}
+
+function onInput(event: Event) {
+    const content = (event.target as HTMLDivElement).textContent;
+    if (content != null && content.trim().length > 0) value.value = +content;
+}
+
+function formatDecimal(d: number) {
+    let v = d.toFixed(2);
+    while (v.endsWith("0")) v = v.substring(0, v.length - 1);
+    if (v.endsWith(".")) v = v.substring(0, v.length - 1);
+    return v;
+}
 </script>
 
 <template>
-    <div class="slider" @pointerdown="onPointerDown">{{ value.toFixed(2) }}</div>
+    <div
+        class="slider"
+        @pointerdown="onPointerDown"
+        @blur="textMode = false"
+        @input="onInput"
+        :contenteditable="textMode"
+    >{{ formatDecimal(value) }}</div>
 </template>
 
 <style scoped lang="scss">
